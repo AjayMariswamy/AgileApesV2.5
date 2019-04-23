6d49e7c91faec9159a63568e9f10f0cb4ae316cf
ajaymariswamy <ajay.mariswamy98@gmail.com>
2019-04-23 15:05:44 +1000

    has some comments and references. Fully functional with Settings page updated

---
+++ app/src/main/java/com/example/agileapes/CorrectAnswerActivity.java
@@ -0,0 +1,28 @@
+package com.example.agileapes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CorrectAnswerActivity extends AppCompatActivity {
+
+    TextView tvCorrectAnswerUserAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correct_answer);
+
+        tvCorrectAnswerUserAnswer = findViewById(R.id.tvCorrectAnswerUserAnswer);
+        tvCorrectAnswerUserAnswer.setText(Global.userAnswer);
+
+    }
+
+    public void goToQuiz(View v) {
+        Intent myIntent = new Intent(getBaseContext(), QuizActivity.class);
+        startActivity(myIntent);
+    }
+}
